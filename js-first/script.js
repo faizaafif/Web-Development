3fc6aec2d1cd920941f03fe15079ab428f6a1e05
Faiza <afiffaiza5@gmail.com>
2025-08-09 11:42:25 +0530

    first js

---
+++ js-first/script.js
@@ -0,0 +1,14 @@
+alert("hello world")
+
+var a = prompt("enter the number");
+var istrue = confirm("do you want to continue ?")
+
+console.log("your number is " + a)
+
+if(istrue){
+    console.log("continue")
+}
+
+else{
+    console.log("stop")
+}
